7e2d0aa63e53b0687361ed7de1fa3f71ff731c18
valentina roa <lvalentinaroa@ucundinamarca.edu.co>
2026-02-27 11:29:57 -0500

    actualizacion de ejercicio con diagrama

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 void main() {
-    Scanner teclado = new Scanner (System.in);
+    Scanner teclado = new Scanner(System.in);
 
 
     Mascota Masco1 = new Mascota();
@@ -11,36 +11,64 @@ void main() {
     Masco1.setPeso(15.4);
     Masco1.setVacunada(false);
 
-    System.out.println(Masco1.toString());
-
-    Mascota Masco2 = new Mascota ("Jacinto", "Camaleon", 15, 15.4, false);
-    System.out.println(Masco2.toString());
 
     Mascota Masco3 = new Mascota();
-    System.out.println("Ingrese el nombre de la mascota");
-    Masco3.setNombre(teclado.next());
-
-    System.out.println("Ingrese la especie de la mascota");
-    Masco3.setEspecie(teclado.next());
-
-    System.out.println("Ingrese la edad de la mascota");
-    Masco3.setEdas(teclado.nextInt());
-
-    System.out.println("Ingrese el peso de la mascota");
-    Masco3.setNombre(teclado.nextDouble());
-
-    System.out.println("Ingrese false o true si esta vacunado la mascota");
-    Masco3.setNombre(teclado.nextBoolean());
 
 
 
+    int opcion;
+    do {
+
+        System.out.println("PETS VETE");
+        System.out.println("1. INFORMACION INICIAL");
+        System.out.println("2. ALIMENTAR");
+        System.out.println("3. VACUNARLA");
+        System.out.println("4. HACER CUMPLIR AÑOS");
+        System.out.println("5. INFORMACION FINAL");
 
 
+        opcion = teclado.nextInt();
 
+        switch (opcion) {
+            case 1 -> {
+                if (Masco3.getNombre() == null) {
+                    Masco3.info();
 
+                    System.out.println("Ingrese el nombre de la mascota: ");
+                    Masco3.setNombre(teclado.next());
 
+                    System.out.println("Ingrese la especie de la mascota: ");
+                    Masco3.setEspecie(teclado.next());
 
+                    System.out.println("Ingrese la edad de la mascota: ");
+                    Masco3.setEdad(teclado.nextInt());
 
+                    System.out.println("Ingrese el peso de la mascota: ");
+                    Masco3.setPeso(teclado.nextDouble());
 
+                    Masco3.setVacunada(false);
+                } else {
+                    Masco3.info();
+                }
+            }
+            case 2 -> {
+                System.out.println("Ingrese la cantidad de alimento (en kg): ");
+                double cantidad = teclado.nextDouble();
+                Masco3.alimentar(cantidad);
+
+            }
+            case 3 -> {
+                Masco3.vacunar();
+
+                System.out.println("VACUNADO");
+            }
+            case 4 ->   Masco3.años();
+            case 5 -> {
+                Masco3.info();
+            }
+            default -> System.out.println("opcion invalida");
+        }
+    } while (opcion != 5);
+    System.out.println("Saliendo");
 
 }
--- src/Mascota.java
+++ src/Mascota.java
@@ -8,20 +8,21 @@ public class Mascota {
     private Boolean vacunada;
 
     //CONSTRUCTOR VACIO
-    public Mascota(){
+    public Mascota() {
 
 
     }
 
     //CONSTRUCTOR CON PARAMETROS
 
-    public Mascota(double vacunada, Boolean peso, int edad, String especie, String nombre) {
-        this.vacunada = vacunada;
-        this.peso = peso;
-        this.edad = edad;
-        this.especie = especie;
+    public Mascota(String nombre, String especie, int edad, double peso) {
         this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+        this.peso = peso;
+        this.vacunada = false;
     }
+
     //GET AND SET
 
     public String getNombre() {
@@ -48,19 +49,90 @@ public class Mascota {
         this.edad = edad;
     }
 
-    public Boolean getPeso() {
+    public double getPeso() {
         return peso;
     }
 
-    public void setPeso(Boolean peso) {
+    public void setPeso(double peso) {
         this.peso = peso;
     }
 
-    public double getVacunada() {
+    public Boolean getVacunada() {
         return vacunada;
     }
 
-    public void setVacunada(double vacunada) {
+    public void setVacunada(Boolean vacunada) {
         this.vacunada = vacunada;
+    }
+
+    @Override
+    public String toString() {
+        return "Mascota{" +
+                "nombre='" + nombre + '\'' +
+                ", especie='" + especie + '\'' +
+                ", edad=" + edad +
+                ", peso=" + peso +
+                ", vacunada=" + vacunada +
+                '}';
+    }
+
+    //METODOS
+
+    //1. VACUNADO
+    public void vacunar() {
+    vacunada = true;
+
+
+        }
+
+
+    //2. ALIMENTAR
+    public void alimentar(double cantidad) {
+        if (peso == 0){
+            System.out.println("NO HAY INFORMACION");
+        }else if (cantidad >= 0) {
+            peso = peso + cantidad;
+            System.out.println("El peso es: " + peso);
+        } else {
+            System.out.println("No cantidades negativas, plis");
+        }
+    }
+
+    //3. AÑOS
+    public void años() {
+        if (edad == 0) {
+            System.out.println("NO HAY INFORMACION");
+        } else {
+            edad = edad + 1;
+            System.out.println("cumplio años, ahora tiene: " + edad + "años");
+        }
+    }
+    //4. MOSTRAR INFO
+    public void info() {
+        if (nombre == null && especie == null && edad == 0 && peso == 0 ) {
+            System.out.println("Nombre: / ");
+            System.out.println("Especie: /");
+            System.out.println("Edad: /");
+            System.out.println("Peso: /");
+            System.out.println("Vacunado: /");
+            System.out.println("  ");
+            System.out.println("INGRESA INFORMACION DE LA MASCOTA:");
+
+
+        } else {
+
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Especie: " + especie);
+            System.out.println("Edad: " + edad);
+            System.out.println("Peso: " + peso);
+
+            if (vacunada == true){
+                System.out.println("Vacunado: SI");
+
+            }else {
+                System.out.println("Vacunado: NO");
+            }
+        }
+
     }
 }
